cd1dbf89b0b4ea4fdf87bfb8280b8b5a2c815429
Bruno Gavioli <brunorgavioli@gmail.com>
2026-04-22 20:22:19 -0300

    feat: nova migration para remover unitPrice de OrderItem

---
+++ GoodHamburger.API/Migrations/20260422232126_Drop_UnitPrice_From_OrderItems.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GoodHamburger.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Drop_UnitPrice_From_OrderItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UnitPrice",
+                table: "OrderItems");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "UnitPrice",
+                table: "OrderItems",
+                type: "TEXT",
+                precision: 10,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
